2fb164f67f5d8adffd2bfba4bc40db6ed32994b5
Aissam Barchil <aissambarchil75@gmail.com>
2022-12-03 12:00:28 +0100

    (chore : parsing ) handle socket creation errors

--- includes/irc.hpp
+++ includes/irc.hpp
@@ -5,6 +5,32 @@
 # include <string>
 # include <vector>
 # include <string>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <arpa/inet.h>
+
+
+
+// #include    <sys/types.h>
+// #include    <netinet/in.h>
+// #include    <arpa/inet.h>
+// #include    <sys/socket.h>
+// #include    <string.h>
+// #include    <iostream>
+// #include    <unistd.h>
+// #include    <stdio.h>
+// #include    <string>
+// #include    <stdlib.h>
+// #include    <cstdlib>
+// #include    <iostream>
+// #include    <string>
+// #include    <fstream>
+// #include    <vector>
+// #include    <map>
+// #include    <cstring>
+
 using namespace std;
 
 
--- main.cpp
+++ main.cpp
@@ -1,25 +1,35 @@
 #include "includes/irc.hpp"
 
-bool is_str_digit(const string &s) {
-	return s.find_first_not_of("0123456789") == string::npos;
+bool is_str_digit(const string &__s) {
+	return __s.find_first_not_of("0123456789") == string::npos;
 }
 
-int parse_arguments(char *av[]) {
-	string __port(av[1]);
-	string __password(av[2]);
+int parse_arguments(const string &__port, const string &__password)
+{
 	if (!is_str_digit(__port) || __password.length() < 4)
 		return -1;
 	return (0);
 }
 
-int main(int ac, char *av[])
+int main(int __ac, char *__av[])
 {
-	if (ac != 3) {
+	int 				__socket_fd;
+	struct sockaddr_in	__server_add = {};
+	if (__ac != 3) {
 		cerr << "Bad command : usage : ./ircserv <port> <password>" << endl;
 		return (1);
 	}
-	if (parse_arguments(av) == -1){
+	string __port(__av[1]), __password(__av[2]);
+	if (parse_arguments(__port, __password) == -1){
 		cerr << "Bad arguments : please enter a valid arguments" << endl;
-	    return (2);
+	    return (1);
 	}
+	if ((__socket_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1 ) {
+		cerr << "Socket error : could not creat socket" << endl;
+		return (1);
+	}
+	__server_add.sin_family = AF_INET;
+	__server_add.sin_port = htons(stoi(__port));
+	cout  << "- Socekt file descriptor :  " << __socket_fd << endl;
+	return (0);
 }
