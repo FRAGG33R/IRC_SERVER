731914d6eee284e6926a1f76d6d2bdb105d949d9
FRAGG33R <aissambarchil75@gmail.com>
2022-12-08 22:01:46 +0100

    (linux: chore ) impliment channels class that contian all channel information that server need

---
+++ includes/Class.channel.hpp
@@ -0,0 +1,17 @@
+#include "main.hpp"
+
+class Channel
+{
+	private:
+		vector<string>	__users;
+		int				__channel_id;
+		int				__users_count;
+		string			__channel_name;
+
+
+	public:
+		Channel();
+		Channel(vector<string> users, int channel_id, int users_coun, string channel_name);
+		Channel(const Channel &obj);
+		~Channel();
+};
